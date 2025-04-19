285e27ec80d826983394d035ed5a91b5460171bd
IliHanSoLow <ilian-ode@gmx.de>
2025-04-19 18:25:26 +0200

    remove trailing spaces on save

--- lua/general/main.lua
+++ lua/general/main.lua
@@ -3,3 +3,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Remove trailing spaces
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
