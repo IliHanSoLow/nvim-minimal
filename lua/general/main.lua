7875fefd4c80136dd2a70db83a9c8d3a4d5a0f77
IliHanSoLow <ilian-ode@gmx.de>
2025-05-03 15:55:34 +0200

    made theming time based

--- lua/general/main.lua
+++ lua/general/main.lua
@@ -3,10 +3,22 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.shell = "nu"
+vim.opt.shell = "bash"
 
 -- Remove trailing spaces
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
+
+local function gammastep_night()
+  local command = "gammastep -p 2>&1| grep 'Night'"
+  local result = os.execute(command)
+  return result == true or result == 0
+end
+
+if gammastep_night() then
+  vim.opt.background = "dark"
+else
+  vim.opt.background = "light"
+end
